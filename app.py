087c546df0ef5a771797bd8d3e31e21f61643b88
KingHacker9000 <Coolioboss7@gmail.com>
2022-06-07 02:59:27 +0400

    cookies

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ db = Database("Database.db")
 
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
